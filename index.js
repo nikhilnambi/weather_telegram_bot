ed3f30d1da4be127ccbacc35a3418e8d18c03241
nikhilnambi <96744136+nikhilnambi@users.noreply.github.com>
2025-05-20 01:24:53 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
